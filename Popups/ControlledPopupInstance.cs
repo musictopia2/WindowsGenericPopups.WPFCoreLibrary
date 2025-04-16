f7b4472c4f6365c0c54fddf1cd3a51d1caf1d27e
Andy Walter <musictopia2@hotmail.com>
2025-04-16 06:58:02 -0700

    Update popup handling and library version
    - Updated `WindowsGenericPopups.CoreLibrary` version to 9.0.4.
    - Added `ControlledPopupInstance` class to manage popup closure.
    - Introduced `ControlledWPFPopupClass` for creating and displaying popups based on display mode.

---
+++ Popups/ControlledPopupInstance.cs
@@ -0,0 +1,12 @@
+﻿namespace WindowsGenericPopups.WPFCoreLibrary.Popups;
+public class ControlledPopupInstance(Window window) : IControlledPopupInstance
+{
+    void IControlledPopupInstance.Close()
+    {
+        // Close the window manually
+        window.Dispatcher.Invoke(() =>
+        {
+            window.Close();
+        });
+    }
+}
---
+++ Popups/ControlledWPFPopupClass.cs
@@ -0,0 +1,24 @@
+﻿namespace WindowsGenericPopups.WPFCoreLibrary.Popups;
+public class ControlledWPFPopupClass : IOpenControlledPopup
+{
+    private Window? _window;
+    private IControlledPopupInstance? _popupInstance;
+    IControlledPopupInstance IOpenControlledPopup.OpenPopup(string message, EnumTimePopupDisplayMode mode)
+    {
+        if (mode == EnumTimePopupDisplayMode.Toast)
+        {
+            _window = new ToastWindow("Toast Message", message);
+        }
+        else
+        {
+            SimplePopupComponent.Message = message;
+            _window = new SimplePopupWindow();
+        }
+
+        _window.Show();
+
+        // Return an instance of a controlled popup so it can be closed later.
+        _popupInstance = new ControlledPopupInstance(_window);
+        return _popupInstance;
+    }
+}
